e640397239acc2c7ad21d0b680b2016a0c9eabc2
royjianxue <royjianxue@yahoo.com>
2023-12-11 15:26:09 -0500

    Trivial updates

--- SymbolTable.py
+++ SymbolTable.py
@@ -1,6 +1,7 @@
 class SymbolTable:
     
     def __init__(self):
+        
         self.symbol_table = {
         'SP': 0, 'LCL': 1, 'ARG': 2, 'THIS': 3, 'THAT': 4, 'R0': 0, 'R1': 1, 'R2': 2,
         'R3': 3, 'R4': 4, 'R5': 5, 'R6': 6, 'R7': 7, 'R8': 8, 'R9': 9, 'R10': 10, 'R11': 11,
