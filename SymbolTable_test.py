905034c8665ff1280df3a264b67aee14b7c58f7a
royjianxue <royjianxue@yahoo.com>
2023-12-11 15:25:59 -0500

    trivial updates

--- SymbolTable_test.py
+++ SymbolTable_test.py
@@ -1,25 +1,26 @@
 import unittest
-from SymbolTable import SymbolTable  #import the SymbolTable class from SymbolTable module
+
+from SymbolTable import SymbolTable  
 
 class TestSymbolTable(unittest.TestCase):
 
     def setUp(self):
         # Initialize a SymbolTable instance for each test
-        self.symbol_table = SymbolTable()
+        self.st = SymbolTable()
 
     def test_contains(self):
-        self.assertTrue(self.symbol_table.contains('SP'))
-        self.assertTrue(self.symbol_table.contains('R7'))
-        self.assertFalse(self.symbol_table.contains('UNKNOWN'))
+        self.assertTrue(self.st.contains('SP'))
+        self.assertTrue(self.st.contains('R7'))
+        self.assertFalse(self.st.contains('UNKNOWN'))
 
     def test_add_entry(self):
-        self.symbol_table.add_entry('LOOP', 16)
-        self.assertTrue(self.symbol_table.contains('LOOP'))
-        self.assertEqual(self.symbol_table.retrive_address('LOOP'), 16)
+        self.st.add_entry('LOOP', 16)
+        self.assertTrue(self.st.contains('LOOP'))
+        self.assertEqual(self.st.retrive_address('LOOP'), 16)
 
     def test_retrieve_address(self):
-        self.assertEqual(self.symbol_table.retrive_address('SCREEN'), 16384)
-        self.assertIsNone(self.symbol_table.retrive_address('UNKNOWN'))
+        self.assertEqual(self.st.retrive_address('SCREEN'), 16384)
+        self.assertIsNone(self.st.retrive_address('UNKNOWN'))
 
 if __name__ == '__main__':
     unittest.main()
