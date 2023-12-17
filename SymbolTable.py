b70ecf2143da086fa16746a2fe1b43b1be7084c7
royjianxue <royjianxue@yahoo.com>
2023-12-16 21:51:49 -0500

    Updated memory counter and add label symbol method

--- SymbolTable.py
+++ SymbolTable.py
@@ -6,11 +6,24 @@ class SymbolTable:
         'SP': 0, 'LCL': 1, 'ARG': 2, 'THIS': 3, 'THAT': 4, 'R0': 0, 'R1': 1, 'R2': 2,
         'R3': 3, 'R4': 4, 'R5': 5, 'R6': 6, 'R7': 7, 'R8': 8, 'R9': 9, 'R10': 10, 'R11': 11,
         'R12': 12, 'R13': 13, 'R14': 14, 'R15': 15, 'SCREEN': 16384, 'KBD': 24576}
+        self.nextMem = 16
    
     def contains(self, symbol):
         return symbol in self.symbol_table
       
-    def add_entry(self, symbol, address):
+    def add_entry(self, symbol):
+        """
+        usage: add variable symbol to the table.
+
+        """
+        self.symbol_table[symbol] = self.nextMem
+        self.nextMem += 1
+    
+    def add_label_entry(self, symbol, address):
+        """
+        usage: add label symbol to the table.
+
+        """
         self.symbol_table[symbol] = address
 
     def retrive_address(self, symbol):
